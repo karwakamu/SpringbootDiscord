8bf3b06fad8e8d062ecd807f6dd20fd285ce656c
Karwakamu <barefoot-92@hotmail.com>
2019-03-23 19:07:27 +0200

    turhaa paskaa pois

--- src/main/java/discord/Schedule.java
+++ src/main/java/discord/Schedule.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Timer;
 import java.util.TimerTask;

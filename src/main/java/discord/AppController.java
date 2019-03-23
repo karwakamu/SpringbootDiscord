24318873417035ccd81ff37cd37c2a1e88c00028
Karwakamu <barefoot-92@hotmail.com>
2019-03-23 17:20:02 +0200

    sylkee 5s välein riviä

--- src/main/java/discord/AppController.java
+++ src/main/java/discord/AppController.java
@@ -1,34 +1,47 @@
 package discord;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Controller
-public class AppController {
+public class AppController
+{
+
+    private static final Logger log = LoggerFactory.getLogger(AppController.class);
+    private static SseEmitter emitter;
+    private Schedule schedule;
+
+    public AppController()
+    {
+        schedule = new Schedule();
+    }
+
+    public void initEmitter ()
+    {
+        emitter = new SseEmitter();
+        emitter.onTimeout(new Runnable(){
+        
+            @Override
+            public void run() {
+                log.info("timeout");
+                initEmitter();
+            }
+        });
+        schedule.addEmitter(emitter);
+    }
 
     @GetMapping("/default")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String defaultGet() {
+        initEmitter();
         return "default";
     }
 
-    private static final Logger log = LoggerFactory.getLogger(AppController.class);
-
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-
-    @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
+    @GetMapping("/sse")
+    public SseEmitter handleSse()
+    {
+        return emitter;
     }
 }
---
+++ src/main/java/discord/Schedule.java
@@ -0,0 +1,70 @@
+package discord;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Schedule
+{
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private static final Logger log = LoggerFactory.getLogger(AppController.class);
+    
+    private Timer timer;
+    private List<SseEmitter> emitters;
+    private ExecutorService nonBlockingService = Executors
+    .newCachedThreadPool();
+
+    public Schedule()
+    {
+        timer = new Timer();
+        emitters = new ArrayList<SseEmitter>();
+        timer.scheduleAtFixedRate(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                reportCurrentTime();
+            }
+        },0,5000);
+    }
+
+    public void reportCurrentTime()
+    {
+        String str = dateFormat.format(new Date());
+
+        for (SseEmitter emitter : emitters) {
+            nonBlockingService.execute(() ->{
+                try
+                {
+                    log.info("emit");
+                    emitter.send(str);
+                }
+                catch(Exception ex)
+                {
+                    log.info("ex");
+                    removeEmitter(emitter);
+                }
+            });
+        }
+    }
+
+    public void addEmitter(SseEmitter emitter)
+    {
+        emitters.add(emitter);
+    }
+
+    public void removeEmitter(SseEmitter emitter)
+    {
+        if(emitters.contains(emitter))
+        emitters.remove(emitter);
+    }
+}
